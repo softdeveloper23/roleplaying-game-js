cb1a757551c48b0b8809e2506f585b80dc76138d
softdeveloper23 <brannongarrett77@gmail.com>
2024-01-30 20:07:06 +0100

    Finished buyHealth function

--- script.js
+++ script.js
@@ -72,7 +72,15 @@ function fightDragon() {
 }
 
 function buyHealth() {
-
+    if (gold >= 10) {
+        gold -= 10;
+        health += 10;
+    
+        goldText.innerText = gold;
+        healthText.innerText = health;
+    } else {
+        text.innerText = "You do not have enough gold to buy health."
+    }
 }
 
 function buyWeapon() {
