1b04f5e297346b4787040e42fced461fee23206f
softdeveloper23 <brannongarrett77@gmail.com>
2024-02-02 20:40:54 +0100

    Added sellWeapon function and code

--- script.js
+++ script.js
@@ -123,6 +123,19 @@ function buyWeapon() {
     }
 }
 
+function sellWeapon() {
+    if (inventory.length > 1) {
+        gold +=15;
+        goldText.innerText = gold;
+        let currentWeapon;
+        currentWeapon = inventory.shift();
+        text.innerText = "You sold a " + currentWeapon + ".";
+        text.innerText += " In your inventory you have: " + inventory;
+    } else {
+        text.innerText = "Don't sell your only weapon!";
+    }
+}
+
 function fightSlime() {
 
 }
